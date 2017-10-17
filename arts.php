952e489da544d8b447693a6fe70cdf722228391c
swiatekm810 <swiatekm810@citrusschools.org>
2017-10-17 11:21:18 -0400

    hello i'm a stupid kid.

---
+++ arts.php
@@ -0,0 +1,152 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Citrus High School</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+		
+		<link type="text/css" media="screen" rel="stylesheet" href="assets/css/responsive-tables.css" />
+		<script type="text/javascript" src="assets/js/responsive-tables.js"></script>
+				
+		<!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="engine0/style.css" />
+<script type="text/javascript" src="engine0/jquery.js"></script>
+
+
+<!-- End WOWSlider.com HEAD section -->
+	</head>
+	
+	<body class="homepage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+			<?php include('header.php'); ?>
+			
+	<div id="main-wrapper">
+					<div id="main" class="container">
+						<div class="row 200%">
+							<div class="12u">
+							
+							<!-- Blog -->
+									<section class="box blog">
+										<h2 class="major"><span>Fine Arts Department</span></h2>
+										<div>
+											<div class="row">
+												<div class="9u 12u(mobile)">
+													<div class="content content-left">
+
+													<div class="container">
+																
+														<div class="column-left"><img src="images/staff/neanderk.jpg" width="53%" height="53%"></br>
+														Kristen Neander</br>
+														Teacher, Department Chair, Drama Coach
+														<ul class="contact">
+															<li><a class="icon fa-twitter" href="http://twitter.com/citrusdrama" target="_blank"><span class="label">Twitter</span></a></li>
+															<li><a class="icon fa-envelope" href="mailto:neanderk@citrus.k12.fl.us" target="_blank"><span class="label">Email</span></a></td></li>
+														</ul>
+														</div>
+														<div class="column-center"><img src="images/staff/boudreau.jpg" width="55%" height="55%"></br>
+														Nancy Boudreau</br>
+														Teacher
+														<ul class="contact">
+															<!--<li><a class="icon fa-twitter" href="http://twitter.com/chscanes" target="_blank"><span class="label">Twitter</span></a></li>-->
+															<li><a class="icon fa-envelope" href="mailto:boudreaun@citrus.k12.fl.us" target="_blank"><span class="label">Email</span></a></td></li>
+														</ul></div>
+														<div class="column-right"><img src="images/staff/edel.jpg" width="55%" height="55%"></br>
+														John Edel</br>
+														Teacher, Choral Director, Breez, Category 5
+														<ul class="contact">
+															<li><a class="icon fa-twitter" href="http://twitter.com/caneschorus" target="_blank"><span class="label">Twitter</span></a></li>
+															<li><a class="icon fa-envelope" href="mailto:edelj@citrus.k12.fl.us" target="_blank"><span class="label">Email</span></a></td></li>
+															<li><a class="icon fa-chrome" href="http://citrushighchorus.webs.com/" target="_blank"><span class="label">CHS Chorus</span></a></td></li>
+														</ul></div>
+													</div>
+													
+													<div class="container">
+																
+														<div class="column-left"><img src="images/staff/journey.jpg" width="53%" height="53%"></br>
+														Phillip Journey</br>
+														Teacher, Boy’s Soccer Head Coach
+														<ul class="contact">
+															<!--<li><a class="icon fa-twitter" href="http://twitter.com/citrusdrama" target="_blank"><span class="label">Twitter</span></a></li>-->
+															<li><a class="icon fa-envelope" href="mailto:journeyp@citrus.k12.fl.us" target="_blank"><span class="label">Email</span></a></td></li>
+														</ul>
+														</div>
+														<div class="column-center"><img src="images/nophotoavailable.png" width="55%" height="55%"></br>
+														William Rankin</br>
+														Teacher, Band Director
+														<ul class="contact">
+															<!--<li><a class="icon fa-twitter" href="http://twitter.com/chscanes" target="_blank"><span class="label">Twitter</span></a></li>-->
+															<li><a class="icon fa-envelope" href="mailto:rankinw@citrus.k12.fl.us" target="_blank"><span class="label">Email</span></a></td></li>
+														</ul></div>
+														<!--<div class="column-right"><img src="images/staff/edel.jpg" width="55%" height="55%"></br>
+														John Edel</br>
+														Teacher, Choral Director, Breez, Category 5
+														<ul class="contact">
+															<li><a class="icon fa-twitter" href="http://twitter.com/caneschorus" target="_blank"><span class="label">Twitter</span></a></li>
+															<li><a class="icon fa-envelope" href="mailto:edelj@citrus.k12.fl.us" target="_blank"><span class="label">Email</span></a></td></li>
+															<li><a class="icon fa-chrome" href="http://citrushighchorus.webs.com/" target="_blank"><span class="label">CHS Chorus</span></a></td></li>
+														</ul></div>-->
+													</div>
+													
+													
+
+													</div>
+													
+												</div>
+											</div>
+										</div>
+									</section>	
+									</div>
+						</div><?php include('footer.php'); ?>
+		
+			</div>
+		</div>
+<!--
+<td>
+<div class="row 200%">
+	<div class="12u">
+<ul class="contact">
+<li><a class="icon fa-twitter" href="http://twitter.com/chscanes" target="_blank"><span class="label">Twitter</span></a></li>
+<li><a class="icon fa-envelope" href="mailto:you@yourdomain.com" target="_blank"><span class="label">Email</span></a></td></li>
+</ul>
+	</div>
+</div>
+<div class="staff_social">
+<td>
+<div class="row 200%">
+	<div class="12u">
+<ul class="contact">
+<li><a class="icon fa-twitter" href="http://twitter.com/chscanes" target="_blank"><span class="label">Twitter</span></a></li>
+<li><a class="icon fa-envelope" href="mailto:you@yourdomain.com" target="_blank"><span class="label">Email</span></a></td></li>
+</ul>
+	</div>
+</div></td>
+</div>
+
+<div class="staff_social">
+<td>
+<div class="row 200%">
+	<div class="12u">
+<ul class="contact">
+<li><a class="icon fa-twitter" href="http://twitter.com/chscanes" target="_blank"><span class="label">Twitter</span></a></li>
+<li><a class="icon fa-envelope" href="mailto:you@yourdomain.com" target="_blank"><span class="label">Email</span></a></td></li>
+</ul>
+	</div>
+</div></td>
+</div>
+</tr>
+</tbody>
+</table>
+	</div>
+</div>
+</div>
+-->	
+
+</body> 
+</html>
